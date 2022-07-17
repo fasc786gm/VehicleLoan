037fa5d93ab527f661a4a8d82108ac72d9333ebe
ritikgupta21092000 <guptaritik349@gmail.com>
2022-07-17 15:55:53 +0530

    Added Struture

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ApplyloanComponent } from './applyloan/applyloan.component';
+import { EmicalculatorComponent } from './emicalculator/emicalculator.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "aboutus",
+    component: AboutComponent
+  },
+  {
+    path: "emiCalculator",
+    component: EmicalculatorComponent
+  },
+  {
+    path: "applyLoan",
+    component: ApplyloanComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { EmicalculatorComponent } from './emicalculator/emicalculator.component';
+import { ApplyloanComponent } from './applyloan/applyloan.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    LoginComponent,
+    RegisterComponent,
+    EmicalculatorComponent,
+    ApplyloanComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
